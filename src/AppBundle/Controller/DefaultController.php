6c32049dc1ac476ad3236f7b7e887adf809b319e
Lu XIN <lu.xin@mybestpro.com>
2018-04-13 15:01:05 +0200

    add data

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class DefaultController extends Controller
 {
@@ -12,11 +13,67 @@ class DefaultController extends Controller
      * HomePage
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, SessionInterface $session)
     {
+        $consume = [''];
+        $transactions = [
+            [
+                'commerce' => "L'escale",
+                'amount' => '-97 €',
+                'createdDate' => '2018-04-13',
+                'createdTime' => '02:25',
+                'category' => 'Bar'
+            ],
+            [
+                'commerce' => "Aubade",
+                'amount' => '-180 €',
+                'createdDate' => '2018-04-13',
+                'createdTime' => '02:25',
+                'category' => 'Vêtement'
+            ],
+            [
+                'commerce' => "Franprix",
+                'amount' => '-14,30 €',
+                'createdDate' => '2018-04-13',
+                'createdTime' => '13:46',
+                'category' => 'SuperMarché'
+            ],
+            [
+                'commerce' => "Louvard",
+                'amount' => '-4,80 €',
+                'createdDate' => '2018-04-13',
+                'createdTime' => '12:25',
+                'category' => 'Restaurant'
+            ],
+        ];
+
+        $cards = [
+            ['type' => 'creditCard', 'bank' => 'visa', 'cardNumber' => '41001210215486', 'category' => ['Vêtement']],
+            [
+                'type' => 'creditCard',
+                'bank' => 'apetitz',
+                'cardNumber' => '41001210215486',
+                'category' => ['Restaurant', 'Bar']
+            ],
+            [
+                'type' => 'creditCard',
+                'bank' => 'mastercard',
+                'cardNumber' => '41001210215486',
+                'category' => ['SuperMarché']
+            ],
+            ['type' => 'creditCard', 'bank' => 'amex', 'cardNumber' => '41001210215486', 'category' => ['Bar']],
+            ['type' => 'fidelityCard', 'bank' => 'visa', 'cardNumber' => '41001210215486', 'category' => ['Vêtement']],
+        ];
+
+        $categories = ['Bar', 'Vêtement', 'Restaurant', 'SuperMarché'];
+
+        $session->set('transactions', $transactions);
+        $session->set('cards', $cards);
+        $session->set('categories', $categories);
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
 }
--- src/AppBundle/Controller/StatController.php
+++ src/AppBundle/Controller/StatController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class StatController extends Controller
 {
@@ -12,11 +13,13 @@ class StatController extends Controller
      * HomePage
      * @Route("/stat", name="stat")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, SessionInterface $session)
     {
+        $transactions = $session->get('transactions');
         // replace this example code with whatever you need
         return $this->render('stat/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'transactions'=> $transactions
         ]);
     }
 }
